74be1f02e0a007427322fb9bb86e5482128d9984
QuentinGalloisDev <quentingallois@yahoo.com>
2023-08-30 18:29:26 +0200

    Modif chemin littéral fetch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,7 +2,7 @@ async function getPhotographers() {
     // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
     // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
 
-    let response = await fetch("../../data/photographers.json")
+    let response = await fetch("././data/photographers.json")
     let { photographers } = await response.json()
 
     //  [
