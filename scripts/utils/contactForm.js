ba03cb3cde655b42ff1637cb6fecf5812ea77042
QuentinGalloisDev <quentingallois@yahoo.com>
2023-09-18 13:32:04 +0200

    Better modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -2,20 +2,31 @@
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    modal.setAttribute("aria-hidden", 'true')
     modal.style.display = "none";
     const background = document.querySelector(".background")
     background.style.display = "none"
+    let main = document.querySelector("#main")
+    main.setAttribute("aria-hidden", 'false')
 }
-
 
 function displayModal() {
     const modal = document.getElementById("contact_modal");
+    modal.setAttribute("aria-hidden", 'false')
     modal.style.display = "block";
     const background = document.querySelector(".background")
     background.style.display = "block"
-
-
-
+    let main = document.querySelector("#main")
+    main.setAttribute("aria-hidden", 'true')
+    let firstChamp = document.getElementById("firstName")
+    //On met le focus sur le premier champ du formmulaire
+    firstChamp.focus()
+    // Quand l'utilisateur appui sur escape, la modal se ferme.
+    modal.addEventListener("keydown", e => {
+        if (e.key === "Escape") {
+            closeModal()
+        }
+    })
 }
 const send = document.querySelector("#submitForm")
 send.addEventListener("click", (event) => {
